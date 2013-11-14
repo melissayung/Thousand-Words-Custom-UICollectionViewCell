b321db08d149f7cf7a8dc206ad233776a57de4e0
Eliot Arntz <arntzel@gmail.com>
2013-11-14 05:17:47 +0100

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Eliot Arntz on 11/14/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
